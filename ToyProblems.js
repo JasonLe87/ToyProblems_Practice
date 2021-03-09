00b4da67c20589eac849abcf97b3d6b06a1bcbc1
Jason Le <jasonle87@gmail.com>
2021-03-09 11:45:04 -0800

    Fizz Buzz

--- ToyProblems.js
+++ ToyProblems.js
@@ -140,3 +140,21 @@ var deleteNode = function(node) {
       node.next = node.next.next
   }
 };
+
+//Fizz Buzz
+
+var fizzBuzz = function(n) {
+  let result = []
+  for (let i = 1; i <= n; i++) {
+      if ( i % 3 === 0 && i % 5 !== 0) {
+          result.push('Fizz')
+      } else if (i % 3 !== 0 && i % 5 === 0) {
+          result.push('Buzz')
+      } else if (i % 3 === 0 && i % 5 === 0) {
+          result.push('FizzBuzz')
+      } else if (i % 3 !== 0 && i % 5 !== 0) {
+          result.push(i.toString())
+      }
+  }
+  return result
+};
